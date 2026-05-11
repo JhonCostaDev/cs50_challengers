de58f25cd49d5e8dc6a6c80f5121a8e36370256e
Jonathan Costa <jhoncosta2@gmail.com>
2026-05-11 11:39:23 -0300

    add the checkBrandCreditCompany function and finish the checkCreditCard function

--- week_1_C/credit/credit.c
+++ week_1_C/credit/credit.c
@@ -3,27 +3,25 @@
 
 int charToInt(char value);
 void checkCreditCard(char arr[]);
+void checkBrandCreditCompany(char arr[], char* stringBuffer);
+//TODO: Create a function that acepts only numbers and max 16 caracters
+//TODO: Refactor the functions to be opmizates
 
 int main(void) {
-    //char cardNumber[17];
+    char cardNumber[17];
 
-    //printf("Number:\n");
-    //scanf("%s", cardNumber);//fgets(cardNumber, 17, stdin);
-    char *cardNumber = "4003600000000014";
-    printf("%s\n", cardNumber);
+    printf("Number: ");
+    scanf("%s", cardNumber);//fgets(cardNumber, 17, stdin);
+    //char *cardNumber = "30569309025904";//"2223000048400011";//"6176292929";//"4003600000000014", "30569309025904";
+    
     checkCreditCard(cardNumber);
-
     
-
-    
-
     return 0;
 }
 
 int charToInt(char value) {
     int number;
     number = value - '0';
-    
     return number;
 }
 void checkCreditCard(char arr[]) {
@@ -31,15 +29,15 @@ void checkCreditCard(char arr[]) {
     int cardNumber[arrLenght];
     int sumEvenPosition = 0;
     int sumOddPosition = 0;
-    int checkSum;
+    int checkSum = 0; 
 
-    //printf("%d\n", arrLenght);
+    
     for(int i = 0; i < arrLenght; i++) {
         
         cardNumber[i] = charToInt(arr[i]);
 
         if(i % 2 ==0) {
-            printf("%d, ", cardNumber[i]);
+            
             if ((cardNumber[i] * 2) > 9) {
                 sumEvenPosition += ((cardNumber[i] * 2)/10) + ((cardNumber[i] * 2)%10);
             } else {
@@ -50,10 +48,31 @@ void checkCreditCard(char arr[]) {
         }
     }
 
-    printf("\n%d\n", sumEvenPosition);
-    printf("\n%d\n", sumOddPosition);
-    printf("%d\n", checkSum);
     checkSum = (sumEvenPosition + sumOddPosition) % 10;
-    printf("%d\n", checkSum);
-    //TODO: implemente the cardbrand
+    
+    char cardBrand[25];
+    if(checkSum != 0) {
+        printf("INVALID\n");
+        return;
+    }
+    checkBrandCreditCompany(arr, cardBrand);
+    printf("%s\n", cardBrand);
 }   
+
+void checkBrandCreditCompany(char arr[], char* stringBuffer) {
+    char brandDigits[3];
+    strncpy(brandDigits, arr, 2);
+    brandDigits[2] = '\0';
+
+    if(strcmp(brandDigits, "30") == 0) {
+        strcpy(stringBuffer, "Diners Club");
+    } else if(strcmp(brandDigits, "37") == 0) {
+        strcpy(stringBuffer, "American Express");
+    } else if(strcmp(brandDigits, "40") == 0 || strcmp(brandDigits, "41") == 0 || strcmp(brandDigits, "42") == 0 || strcmp(brandDigits, "49") == 0) {
+        strcpy(stringBuffer, "VISA");
+    } else if(strcmp(brandDigits, "22") == 0 || strcmp(brandDigits, "51") == 0 || strcmp(brandDigits, "55") == 0)  {
+        strcpy(stringBuffer, "MasterCard");
+    } else {
+        strcpy(stringBuffer, "Unknown Card Company");
+    }
+}
